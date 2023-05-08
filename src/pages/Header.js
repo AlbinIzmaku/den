353d5b951ce2b72203a7a96f2765acec0955ccc0
Albin Izmaku <albinizmaku2@gmail.com>
2023-05-08 20:04:02 +0200

    Update header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -6,28 +6,46 @@ import {
   Typography,
   TextField,
   IconButton,
+  createTheme,
+  ThemeProvider,
+  Avatar,
 } from "@mui/material";
-import { Search } from '@mui/icons-material'
+import { Search } from "@mui/icons-material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9747ff",
+    },
+    secondary: {
+      main: "#000",
+    },
+  },
+});
 
 function Header() {
   return (
-    <Box>
-      <AppBar>
-        <Toolbar display="flex">
-          <Typography>Den</Typography>
-          <TextField
-            placeholder="Search"
-            InputProps={{
-              startAdornment: (
-                <IconButton>
-                  <Search />
-                </IconButton>
-              ),
-            }}
-          />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box color="primary">
+        <AppBar>
+          <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
+            <Typography color="secondary">Den</Typography>
+            <TextField
+              sx={{backgroundColor: 'white', width: '45%'}}
+              placeholder="search for posts, devs..."
+              InputProps={{
+                startAdornment: (
+                  <IconButton>
+                    <Search />
+                  </IconButton>
+                ),
+              }}
+            />
+            <Avatar sx={{ bgcolor: 'white', width: 40, height: 40}}></Avatar>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   );
 }
 
