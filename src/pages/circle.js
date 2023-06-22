2fe65ca5091d1e093e5f83d34decc8a35a28c451
Albin Izmaku <albinizmaku2@gmail.com>
2023-06-22 09:41:29 +0200

    circle

--- src/pages/circle.js
+++ src/pages/circle.js
@@ -1,56 +1,66 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Box } from "@mui/material";
+import * as d3 from "d3";
 
 function Circle() {
+  const svgRef = useRef(null);
   const numLines = 12;
   const angleDiff = 30;
   const circleRadii = [5, 20, 35, 50, 65, 80, 95, 110, 125, 140, 155];
 
-  const renderLines = () => {
-    const lines = [];
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
 
+    // Render circles
+    svg
+      .selectAll("circle")
+      .data(circleRadii)
+      .enter()
+      .append("circle")
+      .attr("cx", 200)
+      .attr("cy", 200)
+      .attr("r", (d) => d)
+      .attr("fill", (d, i) => (i === 0 ? "white" : "none")) // Set first circle fill to white
+      .attr("stroke", "black")
+      .attr("stroke-width", 0.1);
+
+    // Render lines
+    const lines = [];
     for (let i = 0; i < numLines; i++) {
       const angle = (i * angleDiff * Math.PI) / 180;
 
-      circleRadii.forEach((radius) => {
+      circleRadii.slice(1).forEach((radius) => {
+        // Exclude the first circle radius
         const startX = 200 + Math.cos(angle) * radius;
         const startY = 200 + Math.sin(angle) * radius;
         const endX = 200 + Math.cos(angle) * -radius;
         const endY = 200 + Math.sin(angle) * -radius;
 
-        lines.push(
-          <line
-            key={`${radius}-${i}`}
-            x1={startX}
-            y1={startY}
-            x2={endX}
-            y2={endY}
-            stroke="black"
-            strokeWidth="0.5"
-          />
-        );
+        lines.push({
+          startX,
+          startY,
+          endX,
+          endY,
+        });
       });
     }
 
-    return lines;
-  };
+    svg
+      .selectAll("line")
+      .data(lines)
+      .enter()
+      .append("line")
+      .attr("x1", (d) => d.startX)
+      .attr("y1", (d) => d.startY)
+      .attr("x2", (d) => d.endX)
+      .attr("y2", (d) => d.endY)
+      .attr("stroke", "black")
+      .attr("stroke-width", 0.5);
+  }, []);
 
   return (
     <Box sx={{ width: "400px", height: "400px" }}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-        {circleRadii.map((radius, index) => (
-          <circle
-            key={index}
-            cx="200"
-            cy="200"
-            r={radius}
-            fill="none"
-            stroke="black"
-            strokeWidth=".5"
-          />
-        ))}
-        {renderLines()}
-      </svg>
+      <svg ref={svgRef} width="100%" height="100%" />
     </Box>
   );
 }
