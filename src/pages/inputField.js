7055186fb195ea2d80331aebadc8aee41577e902
Albin Izmaku <albinizmaku2@gmail.com>
2023-06-22 16:47:24 +0200

    Updated searchBar

--- src/pages/inputField.js
+++ src/pages/inputField.js
@@ -1,60 +1,50 @@
 import { Search } from "@mui/icons-material";
 import { Container, IconButton, TextField, Typography } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function InputField() {
-  const [searchName, setSearchName] = useState("");
-  const [matchingUsers, setMatchingUsers] = useState([]);
+  const [inputName, setInputName] = useState("");
+  const [matchedName, setMatchedName] = useState(["Albin", "Seladin", "Olti", "Shpetim"]);
+  const [filteredNames, setFilteredNames] = useState([]);
+
+  const inputFieldRef = useRef(null);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users");
-        const data = await response.json();
-        setMatchingUsers(data);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    fetchUsers();
+    inputFieldRef.current.focus();
   }, []);
 
-  const handleSearchName = (event) => {
-    const query = event.target.value.toLowerCase();
-    const matchedUsers = matchingUsers.filter((user) =>
-      user.name.toLowerCase().includes(query)
+  const handleKeyDown = (event) => {
+    const inputLetter = event.target.value;
+    setInputName(inputLetter);
+
+    const filteredNames = matchedName.filter((name) =>
+      name.toLowerCase().includes(inputLetter.toLowerCase())
     );
-    setMatchingUsers(matchedUsers);
-    setSearchName(event.target.value);
+
+    setFilteredNames(filteredNames);
   };
 
   return (
     <>
-      <Container sx={{ position: "relative", width: "45%" }}>
-        <TextField
-          placeholder="search for posts, devs..."
-          InputProps={{
-            style: { background: "white" },
-            startAdornment: (
-              <IconButton>
-                <Search />
-              </IconButton>
-            ),
-          }}
-          value={searchName}
-          onChange={handleSearchName}
-        />
-        {matchingUsers.length > 0 && (
-          <Container sx={{ position: "absolute", top: "100%", left: 0, marginTop: "0.5rem", backgroundColor: "#fff", padding: "0.5rem", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)" }}>
-            {matchingUsers.map((user) => (
-              <Typography key={user.id} variant="body1">
-                {user.name}
-              </Typography>
-            ))}
-          </Container>
-        )}
-      </Container>
+      <TextField
+        placeholder="search for posts, devs..."
+        InputProps={{
+          style: { background: "white" },
+          startAdornment: (
+            <IconButton>
+              <Search />
+            </IconButton>
+          ),
+        }}
+        value={inputName}
+        onChange={handleKeyDown}
+        inputRef={inputFieldRef}
+      />
+      <ul>
+        {filteredNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </>
   );
 }
